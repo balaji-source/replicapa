ff1d6004e0259c61d96825216a170ded461df3c4
sadubalaji.suneel <sadubalaji.suneel@rakuten.com>
2023-07-13 20:11:00 +0530

    source replica is added

---
+++ demo/src/main/java/com/example/demo/resource/Test.java
@@ -0,0 +1,10 @@
+package com.example.demo.resource;
+
+public class Test {
+    public static void main(String[] args) {
+        int size = 22;
+        int divisor = 20;
+
+        System.out.println(size/divisor);
+    }
+}
